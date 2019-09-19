f5b3d441db5ad29e04c8bed86449ba2eb9603ce8
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-19 21:00:30 +0200

    app structure

---
+++ src/component/AddTask.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class AddTask extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div className='form'>
+                <div>add task</div>
+                <hr/>
+            </div>
+        )
+    }
+}
+
+export default AddTask;
