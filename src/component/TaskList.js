c12cb3f907bab2f7b1b6b38bcc5aa334a4150f07
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 14:05:23 +0200

    display finish task in 'task done' section

--- src/component/TaskList.js
+++ src/component/TaskList.js
@@ -3,7 +3,13 @@ import Task from "./Task";
 
 const TaskList = props => {
 
-    const tasks = props.tasks.map(task => <Task key={task.id}
+    const active = props.tasks.filter(task => task.active);
+    const done = props.tasks.filter(task => !task.active);
+    const activeTasks = active.map(task => <Task key={task.id}
+                                                task={task}
+                                                delete={props.delete}
+                                                change={props.change}/>);
+    const doneTasks = done.map(task => <Task key={task.id}
                                                 task={task}
                                                 delete={props.delete}
                                                 change={props.change}/>);
@@ -12,13 +18,14 @@ const TaskList = props => {
         <>
             <div className='active'>
                 <h1>Task to be done</h1>
-                {tasks}
+                {activeTasks.length > 0 ? activeTasks : <p>You have nothing to do</p>}
             </div>
 
             < hr/>
             <div className='done'>
-                <h3>Task done</h3>
-
+                <h3>Task done <em>({doneTasks.length})</em></h3>
+                {doneTasks.length > 5 && <span style={{fontSize: 10}}>Only the last 5 tasks are displayed</span>}
+                {doneTasks.slice(0,5)}
             </div>
 
         </>
