8f4cfacf4acdecda59561b61cfcd2e7b970aa56d
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 15:35:56 +0200

    support for the add function

--- src/component/AddTask.js
+++ src/component/AddTask.js
@@ -17,21 +17,50 @@ class AddTask extends Component {
         })
     };
 
+    handleText = e => {
+        this.setState({
+            text: e.target.value
+        })
+    };
+
+    handleCheckbox = e => {
+        this.setState({
+            checked: e.target.checked
+        })
+    };
+
+    handleClick = e => {
+        const {text, date, checked} = this.state;
+        if(text.length > 2) {
+            const add = this.props.add(text, date, checked);
+            if (add) {
+                this.setState({
+                    text: '',
+                    checked: false,
+                    date: this.minDate
+                })
+            }
+        } else {
+            alert('To short name!!!')
+        }
+    };
+
     render() {
         let maxDate = Number(this.minDate.slice(0, 4)) + 1;
         maxDate = maxDate + '-12-31';
 
         return (
             <div className='form'>
-                <input type='text' placeholder='add task' value={this.state.text}/>
-                <input type='checkbox' checked={this.state.checked}/>
+                <input type='text' placeholder='add task' value={this.state.text}
+                       onChange={this.handleText}/>
+                <input type='checkbox' checked={this.state.checked} onChange={this.handleCheckbox}/>
                 <label htmlFor='important'>Priority</label>
                 <br/>
                 <label htmlFor='date'>Until when to do</label>
                 <input type='date' value={this.state.date} min={this.minDate} max={maxDate}
                        onChange={this.handleDate}/>
                 <br/>
-                <button>Add task</button>
+                <button onClick={this.handleClick}>Add task</button>
             </div>
         )
     }
--- src/component/App.js
+++ src/component/App.js
@@ -4,6 +4,9 @@ import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 
 class App extends Component {
+
+    counter = 9;
+
     state = {
         tasks: [
             {
@@ -96,11 +99,29 @@ class App extends Component {
         });
     };
 
+    addTask = (text, date, important) => {
+        const task = {
+            id: this.counter,
+            text,
+            date,
+            important,
+            active: true,
+            finishDate: null
+        };
+        this.counter++;
+
+        this.setState(prevState => ({
+            tasks: [...prevState.tasks, task]
+        }));
+
+        return true
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>TODO APP</h1>
-                <AddTask/>
+                <AddTask add={this.addTask}/>
                 <TaskList tasks={this.state.tasks} delete={this.deleteTask}
                           change={this.changeTaskStatus}/>
             </div>
