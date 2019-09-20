f859dac90ac216e0b60df3c3a2810bfb1b55e483
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 15:04:40 +0200

    support for text field, checkbox and date selection

--- src/component/AddTask.js
+++ src/component/AddTask.js
@@ -2,21 +2,34 @@ import React, {Component} from 'react'
 import './AddTask.css'
 
 class AddTask extends Component {
+
+    minDate = new Date().toISOString().slice(0, 10);
+
     state = {
         text: '',
         checked: false,
-        date: '2019-09-20'
+        date: this.minDate
+    };
+
+    handleDate = e => {
+        this.setState({
+            date: e.target.value
+        })
     };
 
     render() {
+        let maxDate = Number(this.minDate.slice(0, 4)) + 1;
+        maxDate = maxDate + '-12-31';
+
         return (
             <div className='form'>
                 <input type='text' placeholder='add task' value={this.state.text}/>
-                <input type='checkbox'  checked={this.state.checked}/>
-                <br/>
+                <input type='checkbox' checked={this.state.checked}/>
                 <label htmlFor='important'>Priority</label>
+                <br/>
                 <label htmlFor='date'>Until when to do</label>
-                <input type='date' value={this.state.date} min='2019-01-01' max='2020-12-31'/>
+                <input type='date' value={this.state.date} min={this.minDate} max={maxDate}
+                       onChange={this.handleDate}/>
                 <br/>
                 <button>Add task</button>
             </div>
