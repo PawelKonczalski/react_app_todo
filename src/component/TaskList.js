ecdcc5fac64c680711e2a0db08f16d834a787ad0
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 16:11:41 +0200

    add sort method to list

--- src/component/TaskList.js
+++ src/component/TaskList.js
@@ -5,14 +5,39 @@ const TaskList = props => {
 
     const active = props.tasks.filter(task => task.active);
     const done = props.tasks.filter(task => !task.active);
+
+    if (active.length > 1) {
+        active.sort((a, b) => {
+
+            a = a.date;
+            b = b.date;
+
+            if (a < b) return -1;
+            if (a > b) return 1;
+            return 0
+        });
+    }
+
+    if (done.length > 1) {
+        done.sort((a, b) => {
+            if (a.finishDate < b.finishDate) {
+                return 1
+            }
+            if (a.finishDate > b.finishDate) {
+                return -1
+            }
+            return 0
+        });
+    }
+
     const activeTasks = active.map(task => <Task key={task.id}
-                                                task={task}
-                                                delete={props.delete}
-                                                change={props.change}/>);
+                                                 task={task}
+                                                 delete={props.delete}
+                                                 change={props.change}/>);
     const doneTasks = done.map(task => <Task key={task.id}
-                                                task={task}
-                                                delete={props.delete}
-                                                change={props.change}/>);
+                                             task={task}
+                                             delete={props.delete}
+                                             change={props.change}/>);
 
     return (
         <>
@@ -24,8 +49,9 @@ const TaskList = props => {
             < hr/>
             <div className='done'>
                 <h3>Task done <em>({doneTasks.length})</em></h3>
-                {doneTasks.length > 5 && <span style={{fontSize: 10}}>Only the last 5 tasks are displayed</span>}
-                {doneTasks.slice(0,5)}
+                {doneTasks.length > 5 &&
+                <span style={{fontSize: 10}}>Only the last 5 tasks are displayed</span>}
+                {doneTasks.slice(0, 5)}
             </div>
 
         </>
