4f775146f3722a6ccb463639b12d427d25da5270
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 14:50:48 +0200

    AddTask structure

--- src/component/AddTask.js
+++ src/component/AddTask.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'
+import './AddTask.css'
 
 class AddTask extends Component {
-    state = {};
+    state = {
+        text: '',
+        checked: false,
+        date: '2019-09-20'
+    };
 
     render() {
         return (
             <div className='form'>
-                <div>add task</div>
-                <hr/>
+                <input type='text' placeholder='add task' value={this.state.text}/>
+                <input type='checkbox'  checked={this.state.checked}/>
+                <br/>
+                <label htmlFor='important'>Priority</label>
+                <label htmlFor='date'>Until when to do</label>
+                <input type='date' value={this.state.date} min='2019-01-01' max='2020-12-31'/>
+                <br/>
+                <button>Add task</button>
             </div>
         )
     }
