93e717256c82636f8f90758de1ad32b5bb8aeec9
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 13:42:11 +0200

    deleting and changing task status

--- src/component/App.js
+++ src/component/App.js
@@ -6,23 +6,94 @@ import TaskList from "./TaskList";
 class App extends Component {
     state = {
         tasks: [
-            { id: 1, text: "make a good deed", date: '2020-11-12', important: false, active: true, finishDate: null },
-            { id: 2, text: "paint the house after new year's eve", date: '2019-09-11', important: false, active: true, finishDate: null },
-            { id: 3, text: "lose weight 30 kilograms", date: '2019-05-20', important: true, active: true, finishDate: null },
-            { id: 4, text: "sell beer bottles (20 boxes)", date: '2020-11-12', important: false, active: true, finishDate: null },
-            { id: 5, text: "paint the house again", date: '2019-09-11', important: false, active: true, finishDate: null },
-            { id: 6, text: "hairdresser!!!", date: '2019-05-20', important: true, active: true, finishDate: null },
-            { id: 7, text: "do not receive a registered bailiff", date: '2020-11-12', important: false, active: true, finishDate: null },
-            { id: 8, text: "buy 2 liter bottles", date: '2019-09-11', important: false, active: true, finishDate: null },
+            {
+                id: 1,
+                text: "make a good deed",
+                date: '2020-11-12',
+                important: false,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 2,
+                text: "paint the house after new year's eve",
+                date: '2019-09-11',
+                important: false,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 3,
+                text: "lose weight 30 kilograms",
+                date: '2019-05-20',
+                important: true,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 4,
+                text: "sell beer bottles (20 boxes)",
+                date: '2020-11-12',
+                important: false,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 5,
+                text: "paint the house again",
+                date: '2019-09-11',
+                important: false,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 6,
+                text: "hairdresser!!!",
+                date: '2019-05-20',
+                important: true,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 7,
+                text: "do not receive a registered bailiff",
+                date: '2020-11-12',
+                important: false,
+                active: true,
+                finishDate: null
+            },
+            {
+                id: 8,
+                text: "buy 2 liter bottles",
+                date: '2019-09-11',
+                important: false,
+                active: true,
+                finishDate: null
+            },
         ]
     };
 
-    deleteTask = () => {
+    deleteTask = id => {
+        const tasks = [...this.state.tasks];
+        const index = tasks.findIndex(task => task.id === id);
+        tasks.splice(index, 1);
+        this.setState({
+            tasks
+        })
 
     };
 
-    changeTaskStatus = () => {
-
+    changeTaskStatus = id => {
+        const tasks = [...this.state.tasks];
+        tasks.forEach(task => {
+            if (task.id === id) {
+                task.active = false;
+                task.finishDate = new Date().getTime()
+            }
+        });
+        this.setState({
+            tasks,
+        });
     };
 
     render() {
@@ -30,7 +101,8 @@ class App extends Component {
             <div className="App">
                 TODO APP
                 <AddTask/>
-                <TaskList tasks={this.state.tasks} delete={this.deleteTask} change={this.changeTaskStatus}/>
+                <TaskList tasks={this.state.tasks} delete={this.deleteTask}
+                          change={this.changeTaskStatus}/>
             </div>
         );
     }
